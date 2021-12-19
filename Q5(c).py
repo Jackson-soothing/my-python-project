d5d331c033adf69fa34df87c7f03da701da077be
Jackson <73217199+Jackson-soothing@users.noreply.github.com>
2021-12-19 11:58:37 +0800

    Add files via upload

---
+++ Q5(c).py
@@ -0,0 +1,33 @@
+# i've alr clone the git repo to local, i can deal with that now
+
+import glob
+import re
+
+#get all the files in the desired folder
+files = glob.glob("/Users/soothing/git/my-python-project/*")
+
+
+def check_def(line):
+    m = re.match(r'(^[\s]*def .*$)' , line)
+    if m:
+        return True
+    else:
+        return False
+    
+def count_functions(file_name):
+    
+    f = open(file_name, 'r')
+    
+    count=0
+    
+    for i in f.readlines():
+        if check_def(i):
+            count = count + 1
+            
+    return count
+
+counts = []
+for i in files:
+    counts.append(count_functions(i))
+
+print("Totoal functions defined: " + str(sum(counts)))
