2962639a81ffc0b2630059e77a9dd9efb3481f30
Jackson <73217199+Jackson-soothing@users.noreply.github.com>
2021-12-19 08:59:54 +0800

    Add files via upload
    In a text file, give me total number of appearance of “date” within the text file The date
    format can appears in either one (or multiple) formats shown below:
    a. YYYY/MM/DD
    b. MM/DD/YYYY
    c. DD/MM/YYYY
    d. DD (Jan/Feb/Mar/Apr/May/Jun/Jul/Aug/Sept/Oct/Nov/Dec) YYYY

---
+++ Q6.py
@@ -0,0 +1,23 @@
+import re
+
+#open and read the file
+file = open('my file.txt', 'r')
+text = file.read()
+
+'''
+I expect all the date formats are same as those listed 
+in the question, no strange patterns like 111/01/2019, or 20178/01/09, or
+05 Mon 2020 etc. 
+otherwise the pattern setting in the code 
+should be more precise to match those listed in the question
+'''
+#format a,b,c actually could be treated as the same type
+format_abc = re.findall(r'(\d+/\d+/\d+)', text)
+
+#find format d
+format_d = re.findall(r'(\d+ [a-zA-Z]+ \d+)', text)
+
+#get the total no. of 'date'
+print(len(format_abc) + len(format_d))
+
+file.close()
